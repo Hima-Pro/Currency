2fae86784edbdce49798646e585a66f94fdfb4da
Hima-Pro <456293802@fayoum3.moe.edu.eg>
2023-02-16 23:11:41 +0200

    fix api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
-function exchange(from="EGP",to="RUB",amount=1){
-  fetch(`https://currency-api-tdim.vercel.app/${from}+vs+${to}`)
-  .then(res=>res.json()).then(res=>{
-    var result=Number(res.result);
-    callback([Number(result.toFixed(2)),Number((result*amount).toFixed(2))],from,to);
-  });
-}
-function callback(res,from,to,out=""){
-  if(res[1]>=res[0]){
-    out=`${res[1]+to} - 1${from} = ${res[0]+to}`;
-  } else{
-    out=`1${from} = ${res[0]+to}`;
+async function exchange(from="EGP",to="RUB",amount=1){
+  var response= await fetch(`https://currency-api-tdim.vercel.app/${from}/${to} exchange?amount=`+amount);
+  if (response.ok) {
+    let output = "";
+    let data = await response.json();
+    let result = Number(data.result);
+    let amount = Number(data.amount);
+    if(result==amount){
+      output = `1${from} = ${result+to}`;
+    } else{
+      output = `${amount+to} - 1${from} = ${result+to}`;
+    }
+    window.toVal.value = output;
+  } else {
+    alert("Please check your internet connection !");
   }
-  window.toVal.value=out;
 }
     /*
     const getFlagEmoji = countryCode=>String.fromCodePoint(...[...countryCode.toUpperCase()].map(x=>0x1f1a5+x.charCodeAt()));
